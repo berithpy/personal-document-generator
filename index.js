d76ee5089b93351f5fd28d73886ad8c05d8c488a
Diego Diaz <berith.py@gmail.com>
2019-05-26 16:19:54 -0400

    Add renderer

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-const puppeteer = require("puppeteer");
-const pug = require("pug");
-const fs = require('fs');
+const createPdf = require('./renderer')
 const parser = require('cron-parser');
 
 // This data should be set by a db or manually set everytime.
-const localeOptions = {year: 'numeric', month: 'long', day: 'numeric'}
+const localeOptions = { year: 'numeric', month: 'long', day: 'numeric' }
 const invoiceNumber = 192
-const invoiceDate = new Date().toLocaleDateString('en-US',localeOptions)
+const invoiceDate = new Date().toLocaleDateString('en-US', localeOptions)
 const cronDueDate = "0 0 1 * *"
 let interval = parser.parseExpression(cronDueDate)
-const invoiceDueDate = interval.next().toDate().toLocaleDateString('en-US',localeOptions)
+const invoiceDueDate = interval.next().toDate().toLocaleDateString('en-US', localeOptions)
 
 const senderPicture = "https://placeholder.com/wp-content/uploads/2018/10/placeholder.com-logo1.jpg"
 
@@ -27,12 +25,12 @@ const itemList = [
   { description: "Cooking services", price: 40.76 },
   { description: "Entretainment services", price: 29.76 },
 ]
-let itemTotal=0
-itemList.forEach(item=>{
-  itemTotal+=item.price
+let itemTotal = 0
+itemList.forEach(item => {
+  itemTotal += item.price
   item.price = Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(item.price)
 })
-itemTotal=Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(itemTotal)
+itemTotal = Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(itemTotal)
 const data = {
   invoiceNumber: invoiceNumber,
   invoiceDate: invoiceDate,
@@ -41,49 +39,13 @@ const data = {
   recipient: recipient,
   recipientAddress: recipientAddress,
   recipientCity: recipientCity,
-  recipientEmail:recipientEmail,
+  recipientEmail: recipientEmail,
   sender: sender,
   senderAddress: senderAddress,
   senderEmail: senderEmail,
   senderCity: senderCity,
   itemList: itemList,
   itemTotal: itemTotal
-}
-
-const renderPdf = async function (html) {
-  const content = html
-  const browser = await puppeteer.launch({ headless: true })
-  const page = await browser.newPage()
-  await page.setContent(content)
-  const buffer = await page.pdf({
-    format: 'A4',
-    printBackground: true,
-    margin: {
-      left: '0px',
-      top: '0px',
-      right: '0px',
-      bottom: '0px'
-    }
-  })
-  await browser.close()
-  return buffer
-}
-
-const createPdf = async function (data,pdfFilePath="invoice.pdf") {
-  // Compile the source code
-  const compiledFunction = pug.compileFile("template/invoice.pug");
-  let html = compiledFunction(data);
-  
-  // Render a set of data
-  buffer = await renderPdf(html)
-
-  //chage this to where you want to create the file.
-  fs.writeFile(pdfFilePath, buffer, function (err) {
-    if (err) {
-      return console.log(err);
-    }
-    console.log(`The pdf was saved at ${pdfFilePath}`);
-  });
 }
 
 createPdf(data);
---
+++ renderer.js
@@ -0,0 +1,40 @@
+const puppeteer = require("puppeteer");
+const pug = require("pug");
+const fs = require('fs');
+
+const renderPdf = async function (html) {
+  const content = html
+  const browser = await puppeteer.launch({ headless: true })
+  const page = await browser.newPage()
+  await page.setContent(content)
+  const buffer = await page.pdf({
+    format: 'A4',
+    printBackground: true,
+    margin: {
+      left: '0px',
+      top: '0px',
+      right: '0px',
+      bottom: '0px'
+    }
+  })
+  await browser.close()
+  return buffer
+}
+
+const createPdf = async function (data, pdfFilePath = "invoice.pdf") {
+  // Compile the source code
+  const compiledFunction = pug.compileFile("template/invoice.pug");
+  let html = compiledFunction(data);
+
+  // Render a set of data
+  buffer = await renderPdf(html)
+
+  //chage this to where you want to create the file.
+  fs.writeFile(pdfFilePath, buffer, function (err) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log(`The pdf was saved at ${pdfFilePath}`);
+  });
+}
+module.exports = createPdf
